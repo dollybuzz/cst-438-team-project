2400a9681c4931aa122ea85a27e6561273932433
Tyypos <tyyposofficial@gmail.com>
2020-04-11 08:22:37 -0700

    store disclaimer cookie; styles and comments; added routing to nav; added favicon

--- cst438/src/main/resources/static/js/index.js
+++ cst438/src/main/resources/static/js/index.js
@@ -1,6 +1,17 @@
+// onload
+const onload = () => {
+	// if the user already agreed to the disclaimer
+	// simply show the form when the page is loaded
+	if (getCookie('disclaimer')) {
+		document.querySelector('#show-disclaimer-button').className = 'hidden';
+		document.querySelector('#disclaimer').className = 'hidden';
+		document.querySelector('#symptom-form').className = 'symptom-form shown-block';
+	}
+}
+
 // hide the show-disclaimer-button
 // and show the disclaimer statement block
-function showDisclaimer() {
+const showDisclaimer = () => {
 	document.querySelector('#show-disclaimer-button').className = 'hidden';
 	document.querySelector('#disclaimer').className = 'shown-block';
 }
@@ -8,15 +19,38 @@ function showDisclaimer() {
 // user disagreed to disclaimer
 // show the show-disclaimer-button again
 // and hide the disclaimer statement
-function disagreeDisclaimer() {
+const disagreeDisclaimer = () => {
 	document.querySelector('#show-disclaimer-button').className = 'btn btn-primary shown-inline';
 	document.querySelector('#disclaimer').className = 'hidden';
 }
 
 // user agreed to disclaimer
-// hide the disclaimer statement block
-// and show the symptoms form
-function agreeDisclaimer() {
+// hide the disclaimer statement block and show the symptoms form
+// also sets the disclaimer cookie; note no expiration or way to remove
+const agreeDisclaimer = () => {
 	document.querySelector('#disclaimer').className = 'hidden';
-	document.querySelector('#symptom-form').className = 'shown-block';
+	document.querySelector('#symptom-form').className = 'symptom-form shown-block';
+	document.cookie = 'disclaimer=true';
+}
+
+// thanks to w3schools for this snippet!
+// simple helper function to get cookies
+const getCookie = (cname) => {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+
+    for(var i = 0; i < ca.length; i++) {
+    	var c = ca[i];
+  
+    	while (c.charAt(0) == ' ') {
+    		c = c.substring(1);
+    	}
+  
+    	if (c.indexOf(name) == 0) {
+    		return c.substring(name.length, c.length);
+    	}
+    }
+
+    return "";
 }
