e0091e16044d88b2540026471a57304e633a9d4a
Dalia <43527456+dollybuzz@users.noreply.github.com>
2020-04-13 16:30:50 -1000

    pushing Symptom files, everything is working, still have to do JUnit tests

---
+++ cst438/src/main/java/cst438/service/SymptomService.java
@@ -0,0 +1,46 @@
+package cst438.service;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cst438.domain.Symptom;
+import cst438.domain.SymptomRepository;
+
+@Service
+public class SymptomService {
+	
+		private static final Logger log = LoggerFactory.getLogger(SymptomService.class);
+	
+		@Autowired
+		private SymptomRepository symptomRepository;
+		
+		public List<Symptom> getSymptoms() {
+			if (symptomRepository.findAllByOrderById() != null)
+			{
+				return symptomRepository.findAllByOrderById();
+			}
+			else
+			{
+				log.info("Symptom list not found.");
+				return null;
+			}
+		}
+		
+		public Symptom getSymptomId(long id) {
+			
+			Symptom symp = symptomRepository.findById(id);
+			
+			if(symp != null)
+			{
+				return symp;
+			}
+			else
+			{
+				log.info("Symptom id was not valid.");
+				return null;
+			}
+		}
+}
