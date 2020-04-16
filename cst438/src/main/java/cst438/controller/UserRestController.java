c9d12d48bfcf05e87e51b21de8505cd7bf9066d1
DuskEcho <grebrown@csumb.edu>
2020-04-16 14:54:03 -0700

    added rest insertion for user

--- cst438/src/main/java/cst438/controller/UserRestController.java
+++ cst438/src/main/java/cst438/controller/UserRestController.java
@@ -2,11 +2,16 @@ package cst438.controller;
 
 import java.util.ArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import cst438.domain.*;
 import cst438.service.UserService;
@@ -66,6 +71,47 @@ public class UserRestController {
 		status = users.size() == 0 ? HttpStatus.BAD_REQUEST : HttpStatus.OK;
 		
 		return new ResponseEntity<ArrayList<User>> (users, status);
+	}
+	
+
+	/*
+	 * TODO: Still needs validation for district and country code
+	 */
+	@PostMapping("/api/newUser")
+	public ResponseEntity<User> createSymptomList(
+			@RequestParam String countryCode,
+			@RequestParam String district,
+			@RequestParam Long age,
+			@RequestParam boolean symptOne,
+			@RequestParam boolean symptTwo,
+			@RequestParam boolean symptThree,
+			@RequestParam boolean symptFour,
+			@RequestParam boolean symptFive,
+			@RequestParam boolean symptSix,
+			@RequestParam boolean symptSeven,
+			@RequestParam boolean symptEight,
+			@RequestParam boolean symptNine,
+			@RequestParam boolean symptTen,
+			@RequestParam boolean symptEleven,
+			@RequestParam boolean symptTwelve) {
+		
+		UserSymptomList symptoms = userService.configureSymptoms(
+				symptOne, symptTwo, symptThree, symptFour, 
+				symptFive, symptSix, symptSeven, symptEight, 
+				symptNine, symptTen, symptEleven, symptTwelve);
+		User user = userService.createUser(countryCode, district, symptoms, age);
+		
+		HttpStatus status;
+		
+		if (age <0)// or any of the data is invalid
+		{
+			status = HttpStatus.BAD_REQUEST;
+		}
+		else {
+			status = HttpStatus.OK;
+		}
+		
+		return new ResponseEntity<User> (user, status);
 	}
 	
 }
