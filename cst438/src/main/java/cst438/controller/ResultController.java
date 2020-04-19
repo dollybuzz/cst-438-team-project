90813ad9c3b77fe258f339e467ed7ce023e8fe59
Andrew <ameraz@csumb.edu>
2020-04-19 06:32:08 -0700

    Adding all changes for RabbitMQ to Master

--- cst438/src/main/java/cst438/controller/ResultController.java
+++ cst438/src/main/java/cst438/controller/ResultController.java
@@ -2,10 +2,11 @@ package cst438.controller;
 
 import java.util.List;
 
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,12 @@ public class ResultController {
 	
 	@Autowired
 	private CountryService countryService;
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	private FanoutExchange fanout;
 	
 	private boolean successfulCall;
 
@@ -78,15 +85,13 @@ public class ResultController {
 		int x = 0;
 		int y = 0;
 		String[] symptoms_all = new String[symptoms.size()];
-		int[] symptoms_showing = new int[symptoms.size()];
-		int[] symptoms_not_showing = new int[symptoms.size()];
+		String symptoms_message = "";
 		for(int i = 0; i < symptomVal.length; i++) {
 			if (symptomVal[i] == true) {
-				symptoms_showing[x] = i;
 				x++;
 				symptoms_all[i] = "danger";
+				symptoms_message += " \"Symtom " + x + "\" : \" " + symptoms.get(i).getSymptom() + " \"";
 			}else {
-				symptoms_not_showing[y] = i;
 				y++;
 				symptoms_all[i] = "";
 			}
@@ -102,30 +107,32 @@ public class ResultController {
 			color = "danger";
 			risk = "HIGH RISK";
 		}else if (calculation > 90) {
-			message = "You present a large amount of symptoms. Although this is just checking to see if you ";
+			message = "You present a large amount of symptoms. Although this is just a preliminary check to see if you should seek medical attention, please make sure you call ahead to let the hospital know you are coming and with the symptoms you are presenting.";
 			color = "danger";
 			risk = "MEDIUM-HIGH RISK";
 		}else if (calculation > 60) {
-			message = "";
+			message = "You present symptoms that we would consider in the cusp of possibly having COVID-19. We would recommend to get tested if possible. If not, continue to monitor your symptoms and seek medical attention if you are not feeling well.";
 			color = "warning";
 			risk = "MEDIUM RISK";
 		}else if (calculation > 30) {
-			message = "You present a low risk. Possibly have a cold or something else. ";
+			message = "You present a low risk. Possibly have a cold or something else. However, if you start presenting more symptoms, we recommend to seek medical advise. ";
 			color = "warning";
 			risk = "MEDIUM-LOW RISK";
 		}else if (calculation > 0) {
-			message = "";
+			message = "You do not present many symptoms to be worried as having COVID-19, however, you could still be asymptomatic, and should continue to follow the CDC guidelines of social distancing.";
 			color = "success";
 			risk = "LOW RISK";
 		}else {
-			message = "You do not show any symptoms that would suggest you have COVID-19, however, just because you do not show symptoms, does not mean you do not have the virus. You can be asymptomatic.  ";
+			message = "You do not show any symptoms that would suggest you have COVID-19, however, just because you do not show symptoms, does not mean you do not have the virus. You can be asymptomatic. Please continue to follow CDC guidelines of social distancing.";
 			color = "success";
 			risk = "LOW RISK";
 		}
-
-		model.addAttribute("showing", symptoms_showing);
+		
+		String msg = "{\"email\": \"cdc@doesnotexists.org\" \"msg\": \" " + message + "\" " + symptoms_message + " }";
+		System.out.println("Message being sent to CDC: " + msg);
+		
+		rabbitTemplate.convertSendAndReceive(fanout.getName(), "", msg);
 		model.addAttribute("symptoms_all", symptoms_all);
-		model.addAttribute("notShowing", symptoms_not_showing);
 		model.addAttribute("success", true);
 		model.addAttribute("cityName", cityName);
 		model.addAttribute("countryName", countryName);
